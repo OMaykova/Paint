c01e79ccda15b9f47992d1fc8ce602564e6a25e1
Olga Maykova <dolkaaaaa@gmail.com>
2023-04-25 12:09:00 +0300

    feat:structure

---
+++ src/components/SettingBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/settingbar.scss';
+
+const SettingBar = () => {
+  return (
+    <div className='settingbar'></div>
+  );
+}
+
+export default SettingBar;
