7e2c279f1b33c5a1559ace744e2301c4081df807
Olga Maykova <dolkaaaaa@gmail.com>
2023-04-26 17:24:55 +0300

    feat: brush

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/canvas.scss';
+import { observer } from 'mobx-react-lite';
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
 
-const Canvas = () => {
-	return (
-		<div className='canvas'>
-			<canvas width={600} height={400}></canvas>
-		</div>
-	);
-}
+const Canvas = observer(() => {
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current)
+    // toolState.setTool(new Brush(canvasRef.current))
+  }, [])
+
+
+  return (
+    <div className='canvas'>
+      <canvas ref={canvasRef} width={600} height={400}>
+        "The drawing desktop should be displayed here. If you see this text, it means that your browser does not support this technology."
+      </canvas>
+    </div>
+  );
+})
 
 export default Canvas;
